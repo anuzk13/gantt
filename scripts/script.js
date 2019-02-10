c7164ed5a2c432d6912255c87757afc2e473ce07
Ana Cardeas <am.cardenas927@gmail.com>
2019-02-10 14:34:17 -0500

    working

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,58 @@
 google.charts.load('current', {'packages':['gantt']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawGID);
 
-function daysToMilliseconds(days) {
-  return days * 24 * 60 * 60 * 1000;
+function toMilliseconds(minutes) {
+  return minutes * 60 * 1000;
 }
 
-function drawChart() {
+function drawChart(data) {
 
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Task ID');
-  data.addColumn('string', 'Task Name');
-  data.addColumn('string', 'Resource');
-  data.addColumn('date', 'Start Date');
-  data.addColumn('date', 'End Date');
-  data.addColumn('number', 'Duration');
-  data.addColumn('number', 'Percent Complete');
-  data.addColumn('string', 'Dependencies');
+  var otherData = new google.visualization.DataTable();
+  otherData.addColumn('string', 'Task ID');
+  otherData.addColumn('string', 'Task Name');
+  otherData.addColumn('string', 'Resource');
+  otherData.addColumn('date', 'Start');
+  otherData.addColumn('date', 'End');
+  otherData.addColumn('number', 'Duration');
+  otherData.addColumn('number', 'Percent Complete');
+  otherData.addColumn('string', 'Dependencies');
 
-  data.addRows([
-    ['Research', 'Find sources', null,
-     new Date(2015, 0, 1), new Date(2015, 0, 5), null,  100,  null],
-    ['Write', 'Write paper', 'write',
-     null, new Date(2015, 0, 9), daysToMilliseconds(3), 25, 'Research,Outline'],
-    ['Cite', 'Create bibliography', 'write',
-     null, new Date(2015, 0, 7), daysToMilliseconds(1), 20, 'Research'],
-    ['Complete', 'Hand in paper', 'complete',
-     null, new Date(2015, 0, 10), daysToMilliseconds(1), 0, 'Cite,Write'],
-    ['Outline', 'Outline paper', 'write',
-     null, new Date(2015, 0, 6), daysToMilliseconds(1), 100, 'Research']
+  otherData.addRows([
+    ['toTrain', 'Walk to train stop', 'walk', null, null, toMilliseconds(5), 100, null],
+    ['music', 'Listen to music', 'music', null, null, toMilliseconds(70), 100, null],
+    ['wait', 'Wait for train', 'wait', null, null, toMilliseconds(10), 100, 'toTrain'],
+    ['train', 'Train ride', 'train', null, null, toMilliseconds(45), 75, 'wait'],
+    ['toWork', 'Walk to work', 'walk', null, null, toMilliseconds(10), 0, 'train'],
+    ['work', 'Sit down at desk', null, null, null, toMilliseconds(2), 0, 'toWork'],
   ]);
 
   var options = {
-    height: 275
+    height: 800,
+    gantt: {
+      defaultStartDateMillis: new Date(2015, 3, 28)
+    }
   };
 
   var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
 
   chart.draw(data, options);
+}
+
+function drawGID() {
+  var queryString = encodeURIComponent('SELECT A, B, C, D, E, F, G, H');
+
+  var query = new google.visualization.Query(
+      'https://docs.google.com/spreadsheets/d/1glZdhRuIK_BR9GDv2vtH1Gez6bqPfKtOAOt52ND_9DI/gviz/tq?gid=0&headers=1&tq=' + queryString);
+  query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+  if (response.isError()) {
+    alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  }
+
+  var data = response.getDataTable();
+  console.log(data)
+  drawChart(data)
 }
